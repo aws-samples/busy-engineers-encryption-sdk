711a553a908970f0151be19e40ef4f3c5c922813
Matt Bullock <bullocm@amazon.com>
2018-10-02 11:14:58 -0700

    exercise 0 starting point (#8)

--- src/reinvent_sid345/encrypt_decrypt.py
+++ src/reinvent_sid345/encrypt_decrypt.py
@@ -14,11 +14,7 @@
 
 This is the only module that you need to modify in the SID345 workshop.
 """
-import base64
 import json
-import time
-
-import aws_encryption_sdk
 
 
 class EncryptDecrypt(object):
@@ -29,11 +25,7 @@ class EncryptDecrypt(object):
         self._message_type = "message_type"
         self._type_order_inquiry = "order inquiry"
         self._timestamp = "rough timestamp"
-        master_key_provider = aws_encryption_sdk.KMSMasterKeyProvider(key_ids=[key_id])
-        cache = aws_encryption_sdk.LocalCryptoMaterialsCache(capacity=100)
-        self.materials_manager = aws_encryption_sdk.CachingCryptoMaterialsManager(
-            cache=cache, master_key_provider=master_key_provider, max_age=5.0 * 60.0, max_messages_encrypted=10
-        )
+        self.key_id = key_id
 
     def encrypt(self, data):
         """Encrypt data.
@@ -42,14 +34,7 @@ class EncryptDecrypt(object):
         :returns: Base64-encoded, encrypted data
         :rtype: str
         """
-        encryption_context = {
-            self._message_type: self._type_order_inquiry,
-            self._timestamp: str(int(time.time() / 3600.0)),
-        }
-        ciphertext, _header = aws_encryption_sdk.encrypt(
-            source=json.dumps(data), materials_manager=self.materials_manager, encryption_context=encryption_context
-        )
-        return base64.b64encode(ciphertext).decode("utf-8")
+        return json.dumps(data)
 
     def decrypt(self, data):
         """Decrypt data.
@@ -57,13 +42,4 @@ class EncryptDecrypt(object):
         :param bytes data: Base64-encoded, encrypted data
         :returns: JSON-decoded, decrypted data
         """
-        ciphertext = base64.b64decode(data)
-        plaintext, header = aws_encryption_sdk.decrypt(source=ciphertext, materials_manager=self.materials_manager)
-
-        try:
-            if header.encryption_context[self._message_type] != self._type_order_inquiry:
-                raise KeyError()  # overloading KeyError to use the same exit whether wrong or missing
-        except KeyError:
-            raise ValueError("Bad message type in decrypted message")
-
-        return json.loads(plaintext)
+        return json.loads(data)
