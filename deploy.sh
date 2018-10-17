56bd93f8bb3588e1bfd15fecead0b010be5d2c8e
mattsb42-aws <bullocm@amazon.com>
2018-10-17 14:39:05 +0100

    * remove environment variable resets * disable redirect overlay

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-set -x
-set -u
-set -e
-
 BUCKET=sid345.reinvent-workshop.com
 CFDISTRO=E11MLVAL7KJ1N2
 
 aws s3 sync --acl public-read docs/build/html s3://$BUCKET
-(cd docs/build/html; for i in */index.html; do
-    aws s3 cp --acl public-read --website-redirect /$i $i s3://$BUCKET/`dirname $i`
-done)
+#(cd docs/build/html; for i in */index.html; do
+#    aws s3 cp --acl public-read --website-redirect /$i $i s3://$BUCKET/`dirname $i`
+#done)
 
 aws configure set preview.cloudfront true
 aws cloudfront create-invalidation --distribution-id $CFDISTRO --paths '/*'
