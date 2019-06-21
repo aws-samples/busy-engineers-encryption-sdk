3a73f27fb00deed1d7ab0ca3411364502ce672f8
Samreen <sfk@amazon.com>
2019-06-21 10:37:09 -0700

    propagating changes from e5

--- src/busy_engineers_workshop/encrypt_decrypt.py
+++ src/busy_engineers_workshop/encrypt_decrypt.py
@@ -21,12 +21,12 @@ import json
 class EncryptDecrypt(object):
     """Encrypt and decrypt data."""
 
-    def __init__(self, key_id):
+    def __init__(self, key_id_east, key_id_west):
         """Set up materials manager and static values."""
         self._message_type = "message_type"
         self._type_order_inquiry = "order inquiry"
         self._timestamp = "rough timestamp"
-        self.key_id = key_id
+        self.key_id = key_id_east
 
     def encrypt(self, data):
         """Encrypt data.
