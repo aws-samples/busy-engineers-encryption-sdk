27e19aef0bce169731ab961753b886adb11b5ab7
lizroth <30636882+lizroth@users.noreply.github.com>
2019-06-21 10:44:44 -0700

    Add Maven commands for grant deployment.
    Adds support for users to invoke `mvn deploy -P"assign-grant"` and `mvn deploy -P"revoke-grant"`.

---
+++ deploy-plugin/src/main/java/sample/RevokeGrantPlugin.java
@@ -0,0 +1,80 @@
+package sample;
+
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.identitymanagement.AmazonIdentityManagement;
+import com.amazonaws.services.identitymanagement.AmazonIdentityManagementClientBuilder;
+import com.amazonaws.services.kms.AWSKMS;
+import com.amazonaws.services.kms.AWSKMSClientBuilder;
+import com.amazonaws.services.kms.model.DescribeKeyRequest;
+import com.amazonaws.services.kms.model.DescribeKeyResult;
+import com.amazonaws.services.kms.model.RevokeGrantRequest;
+import com.amazonaws.services.kms.model.RevokeGrantResult;
+import com.amazonaws.services.securitytoken.AWSSecurityTokenService;
+import com.amazonaws.services.securitytoken.AWSSecurityTokenServiceClientBuilder;
+import com.amazonaws.services.securitytoken.model.GetCallerIdentityRequest;
+import com.amazonaws.services.securitytoken.model.GetCallerIdentityResult;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+
+@Mojo(name="revoke-grant")
+public class RevokeGrantPlugin extends AbstractMojo {
+    @Parameter(property="grantId.path") protected String grantIdPath;
+    
+    @Override
+    public void execute() throws MojoFailureException {
+        try {
+            File grantIdFile = new File(grantIdPath);
+            if (!grantIdFile.exists()) {
+                getLog().warn("Grant has not yet been assigned (do you need to 'assign-grant'?).");
+                return;
+            }
+            
+            String grantId;
+            
+            //Load the grant ID to revoke
+            try (BufferedReader grantIdReader = new BufferedReader(new FileReader(grantIdFile))) {
+                grantId = grantIdReader.readLine();
+            }
+            
+            // Set up clients
+            AWSSecurityTokenService stsClient = AWSSecurityTokenServiceClientBuilder.defaultClient();
+            AWSKMS kmsClient = AWSKMSClientBuilder.standard().withRegion(Regions.US_WEST_2).build();
+            AmazonIdentityManagement iamClient = AmazonIdentityManagementClientBuilder.defaultClient();
+            
+            // Get the accountId we're running in
+            GetCallerIdentityResult identityResult = stsClient.getCallerIdentity(new GetCallerIdentityRequest());
+            String accountId = identityResult.getAccount();
+            
+            // Map the alias to the real CMK ARN
+            String aliasArn = "arn:aws:kms:us-west-2:" + accountId + ":alias/busy-engineers-workshop-us-west-2-key";
+            DescribeKeyRequest descRequest = new DescribeKeyRequest()
+                                                    .withKeyId(aliasArn);
+            DescribeKeyResult descResult = kmsClient.describeKey(descRequest);
+            String keyArn = descResult.getKeyMetadata().getArn();
+            getLog().info("Key ARN " + keyArn + " for alias " + aliasArn);
+            
+            // Revoke the grant on the CMK
+            RevokeGrantRequest grantRequest = new RevokeGrantRequest()
+                                                    .withKeyId(keyArn)
+                                                    .withGrantId(grantId);
+
+            RevokeGrantResult grantResult = kmsClient.revokeGrant(grantRequest);
+
+            getLog().info("Revoked grant " + grantId);
+            
+            grantIdFile.delete();
+            getLog().info("Cleaned up grant ID file.");
+        } catch (RuntimeException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new MojoFailureException("Unexpected error", e);
+        }
+    }
+}
