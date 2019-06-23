65fefd83c1cc0bc01c52d5df79b0ada730d6b3f9
lizroth <30636882+lizroth@users.noreply.github.com>
2019-06-23 13:07:24 -0700

    Spruce up assign/revoke grant scripts.
    * Exit script on error
    * Test for grant ID file before proceeding
    * Match down the role more specifically to the extended environment Lambda permissions
    * Reduce grant permissions for least privilege goodness

--- build-tools/revoke_grant.sh
+++ build-tools/revoke_grant.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+set -e
+
+if [ ! -f build-tools/grant_id.txt ]; then
+    >&2 echo "No grant ID found to revoke! (Do you need to 'assign-grant' first?)"
+    exit 1
+fi
+
 echo "********************"
-echo "* Revoking Grants *"
+echo "* Revoking Grants  *"
 echo "********************"
 
 account=$(aws sts get-caller-identity --query 'Account' --output text)
@@ -11,4 +18,4 @@ key_id=$(aws kms describe-key --key-id $arn --region "us-west-2" --query 'KeyMet
 grant_id=$(<build-tools/grant_id.txt)
 revoke=$(aws kms revoke-grant --key-id $key_id --region "us-west-2" --grant-id $grant_id)
 echo "grant_id successfully revoked...$revoke"
-rm -rf build-tools/grant_id.txt
+rm build-tools/grant_id.txt
